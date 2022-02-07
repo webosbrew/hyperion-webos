4a3e1f39939eb9f1a8fedb87d9c8dcf7a2080717
Piotr Dobrowolski <admin@tastycode.pl>
2022-02-07 22:13:12 +0100

    libvtcapture: delay startup until capture_start

--- src/backends/libvtcapture.cpp
+++ src/backends/libvtcapture.cpp
@@ -15,6 +15,7 @@ typedef struct _vtcapture_backend_state {
     VT_DRIVER *driver; // = NULL;
     VT_CLIENTID_T client[128];
     _LibVtCaptureBufferInfo buff;
+    _LibVtCaptureProperties props;
     char *curr_buff;
     bool terminate;
 } vtcapture_backend_state_t;
@@ -25,11 +26,6 @@ int capture_terminate(void* state);
 int capture_init(cap_backend_config_t* config, void** state_p)
 {
     int ret = 0;
-    VT_RESOLUTION_T resolution = {config->resolution_width, config->resolution_height};
-    VT_DUMP_T dump = 1;
-    VT_LOC_T loc = {0, 0};
-    VT_BUF_T buf_cnt = 3;
-    const VT_CALLER_T *caller = "hyperion-webos_service";
 
     INFO("Starting vtcapture initialization.");
 
@@ -47,69 +43,14 @@ int capture_init(cap_backend_config_t* config, void** state_p)
 
     INFO("Driver created!");
 
-    sprintf(self->client, "%s", "00");
-
-    if ((ret = vtCapture_init(self->driver, caller, self->client)) == 17) {
-
-        ERR("vtCapture_init not ready yet return: %d", ret);
-        ret = -2; goto err_destroy;
-    }
-    else if (ret == 11) {
-
-        ERR("vtCapture_init failed: %d Permission denied! Quitting...", ret);
-        ret = -3; goto err_destroy;
-    }
-    else if (ret != 0) {
-
-        ERR("vtCapture_init failed: %d Quitting...", ret);
-        ret = -4; goto err_destroy;
-    }
-    INFO("vtCapture_init done! Caller_ID: %s Client ID: %s", caller, self->client);
-
-    _LibVtCaptureProperties props;
-
     // Sorry, no unlimited fps for you.
-    if (config->fps == 0)
-        config->fps = 60;
-
-    INFO("fps = %d", config->fps);
-
-    props.dump = dump;
-    props.loc = loc;
-    props.reg = resolution;
-    props.buf_cnt = buf_cnt;
-    props.frm = config->fps;
-
-    if ((vtCapture_preprocess(self->driver, self->client, &props)) == 1) {
-
-        ERR("vtCapture_preprocess not ready yet return: %d", ret);
-        ret = -5; goto err_destroy;
-    }
-    else if (ret != 0) {
-        
-        ERR("vtCapture_preprocess failed: %x Quitting...", ret);
-        ret = -6; goto err_destroy;
-    }
-    INFO("vtCapture_preprocess done!");
-
-    _LibVtCapturePlaneInfo plane;
-    if ((vtCapture_planeInfo(self->driver, self->client, &plane)) != 0 ) {
-
-        ERR("vtCapture_planeInfo failed: %xQuitting...", ret);
-        ret = -7; goto err_destroy;
-    }
-
-    self->width = plane.planeregion.c;
-    self->height = plane.planeregion.d;
-    self->stride = plane.stride;
-
-    INFO("vtCapture_planeInfo done! stride: %d Region: x: %d, y: %d, w: %d, h: %d Active Region: x: %d, y: %d w: %d h: %d", 
-            plane.stride, plane.planeregion.a, plane.planeregion.b, plane.planeregion.c, plane.planeregion.d, 
-            plane.activeregion.a, plane.activeregion.b, plane.activeregion.c, plane.activeregion.d);
-
-    self->terminate = false;
-
-    INFO("vtcapture initialization finished.");
+    self->props.frm = config->fps == 0 ? 60 : config->fps;
+    self->props.dump = 1;
+    self->props.loc.x = 0;
+    self->props.loc.y = 0;
+    self->props.reg.w = config->resolution_width;
+    self->props.reg.h = config->resolution_height;
+    self->props.buf_cnt = 3;
 
     *state_p = self;
 
@@ -117,9 +58,6 @@ int capture_init(cap_backend_config_t* config, void** state_p)
 
 err_destroy:
     if (self->driver) {
-        
-        vtCapture_postprocess(self->driver, self->client);
-        vtCapture_finalize(self->driver, self->client);
         vtCapture_release(self->driver);
     }
     free(self);
@@ -142,45 +80,89 @@ int capture_cleanup(void* state)
 int capture_start(void* state)
 {
     int ret;
+    const VT_CALLER_T *caller = "hyperion-webos_service";
     vtcapture_backend_state_t* self = (vtcapture_backend_state_t*) state;
+
+    sprintf(self->client, "%s", "00");
+
+    if ((ret = vtCapture_init(self->driver, caller, self->client)) == 17) {
+
+        ERR("vtCapture_init not ready yet return: %d", ret);
+        ret = -2; goto err_init;
+    }
+    else if (ret == 11) {
+
+        ERR("vtCapture_init failed: %d Permission denied! Quitting...", ret);
+        ret = -3; goto err_init;
+    }
+    else if (ret != 0) {
+
+        ERR("vtCapture_init failed: %d Quitting...", ret);
+        ret = -4; goto err_init;
+    }
+    INFO("vtCapture_init done! Caller_ID: %s Client ID: %s", caller, self->client);
+
+    if ((vtCapture_preprocess(self->driver, self->client, &self->props)) == 1) {
+
+        ERR("vtCapture_preprocess not ready yet return: %d", ret);
+        ret = -5; goto err_preprocess;
+    }
+    else if (ret != 0) {
+        ERR("vtCapture_preprocess failed: %x Quitting...", ret);
+        ret = -6; goto err_preprocess;
+    }
+    INFO("vtCapture_preprocess done!");
+
+    _LibVtCapturePlaneInfo plane;
+    if ((vtCapture_planeInfo(self->driver, self->client, &plane)) != 0 ) {
+
+        ERR("vtCapture_planeInfo failed: %xQuitting...", ret);
+        ret = -7; goto err_planeinfo;
+    }
+
+    self->width = plane.planeregion.c;
+    self->height = plane.planeregion.d;
+    self->stride = plane.stride;
+
+    INFO("vtCapture_planeInfo done! stride: %d Region: x: %d, y: %d, w: %d, h: %d Active Region: x: %d, y: %d w: %d h: %d", 
+            plane.stride, plane.planeregion.a, plane.planeregion.b, plane.planeregion.c, plane.planeregion.d, 
+            plane.activeregion.a, plane.activeregion.b, plane.activeregion.c, plane.activeregion.d);
+
+    self->terminate = false;
+
+    INFO("vtcapture initialization finished.");
 
     if ((vtCapture_process(self->driver, self->client)) != 0) {
 
         ERR("vtCapture_process failed: %xQuitting...", ret);
-        return -1;
+        ret = -1; goto err_process;
     }
     INFO("vtCapture_process done!");
 
-    int cnter = 0;
-    do {
-
-        usleep(100000);
-
-        if ((ret = vtCapture_currentCaptureBuffInfo(self->driver, &self->buff)) == 0 ) {
-
-            break;
-        }
-        else if (ret != 2) {
-
-            ERR("vtCapture_currentCaptureBuffInfo failed: %x Quitting...", ret);
-            capture_terminate(state);
-            return -1;
-        }
-        cnter++;
-    } while(ret != 0);
-
-    INFO("vtCapture_currentCaptureBuffInfo done after %d tries! addr0: %p addr1: %p size0: %d size1: %d", 
-            cnter, self->buff.start_addr0, self->buff.start_addr1, self->buff.size0, self->buff.size1);
-
     return 0;
+
+err_process:
+    vtCapture_stop(self->driver, self->client);
+
+err_planeinfo:
+    vtCapture_postprocess(self->driver, self->client);
+
+err_preprocess:
+    vtCapture_finalize(self->driver, self->client);
+
+err_init:
+    return ret;
 }
 
 int capture_terminate(void* state)
-{    
+{
     vtcapture_backend_state_t* self = (vtcapture_backend_state_t*) state;
 
     self->terminate = true;
+
     vtCapture_stop(self->driver, self->client);
+    vtCapture_postprocess(self->driver, self->client);
+    vtCapture_finalize(self->driver, self->client);
 
     return 0;
 }
