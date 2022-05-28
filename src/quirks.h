b81163edeb9f2ed79ecb687c6f3aeee748843d4a
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-05-28 02:01:53 +0200

    DILE_VT/Quirks: Implement QUIRK_DILE_VT_NO_FREEZE_CAPTURE, Do not wait for DILE_VT_SetVideoFrameOutputDeviceState: FREEZED in capture_acquire_frame

--- src/quirks.h
+++ src/quirks.h
@@ -3,5 +3,6 @@
 #define HAS_QUIRK(quirk_val, enum_val) ((quirk_val & enum_val) == enum_val)
 
 enum CAPTURE_QUIRKS {
-    QUIRK_DILE_VT_CREATE_EX = 0x1
+    QUIRK_DILE_VT_CREATE_EX = 0x1,
+    QUIRK_DILE_VT_NO_FREEZE_CAPTURE = 0x2
 };
