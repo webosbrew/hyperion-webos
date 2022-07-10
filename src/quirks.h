448b54a5fc4bed232415f1fd5723aa3a9f83d194
TBSniller <tbsniller@gmail.com>
2022-07-10 23:03:20 +0200

    Document QUIRK_VTCAPTURE_K6HP_FORCE_CAPTURE

--- src/quirks.h
+++ src/quirks.h
@@ -12,5 +12,6 @@ enum CAPTURE_QUIRKS {
        DILE_VT_SetVideoFrameOutputDeviceOutputRegion
     */
     QUIRK_DILE_VT_DUMP_LOCATION_2 = 0x4,
+    // Reenables video capture on specific TVs using kernel module: https://github.com/TBRlyNotMe/force-capture-interface
     QUIRK_VTCAPTURE_K6HP_FORCE_CAPTURE = 0x8
 };
